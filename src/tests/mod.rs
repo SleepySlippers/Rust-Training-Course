1b717235c0f998268b86279ce4447cfe2c091e0e
Arseny Staroverov <arseny.staroverov@gmail.com>
2025-11-02 12:28:35 +0300

    Make test and tasks toggleble to analyze
    Now you can just uncomment lines in Makefile to turn on tasks to analyze
    only them instead of all not ready tasks
    
    It also includes check that list of tasks is not empty
    
    Also, two targets are added:
    - test => which runs tests
    - all => which runs lint and test targets

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,11 +1,22 @@
+#[cfg(feature = "c1_common_concepts")]
 mod c1;
+#[cfg(feature = "c10_concurrency")]
 mod c10;
+#[cfg(feature = "c11_async")]
 mod c11;
+#[cfg(feature = "c12_unsafe_advanced_traits_macros")]
 mod c12;
+#[cfg(feature = "c3_ownership_and_memory")]
 mod c3;
+#[cfg(feature = "c4_structs_methods_enums_pattern_matching")]
 mod c4;
+#[cfg(feature = "c5_collections")]
 mod c5;
+#[cfg(feature = "c6_error_handling_tests_docs")]
 mod c6;
+#[cfg(feature = "c7_generics_traits_lifetimes")]
 mod c7;
+#[cfg(feature = "c8_iterators_and_closures")]
 mod c8;
+#[cfg(feature = "c9_smart_pointers")]
 mod c9;
